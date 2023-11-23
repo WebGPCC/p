48f42e6d50ae58b3f25fd88b637ad8f0203e5d14
WebGPCC <webdevpcc@gmail.com>
2023-11-23 10:09:29 +0100

    Add mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ const Navbar = () => {
   return (
     <>
         <nav className="absolute left-0 py-6 w-full transition-all duration-300 z-30 lg:px-10  top-5">
+        <div className="w-full px-5 lg:px-10 flex items-center justify-between absolute top-5 lg:hidden">
+            <a aria-current="page" href="#" className="router-link-active router-link-exact-active"><img src="https://www.fundamental.bg/logo-white.svg" className="h-5 mt-1" alt="Fundamental Studio Ltd"/></a><a className="cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 h-8 hover:text-[#3EC091] transition-all duration-300 text-white"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path></svg></a></div>
 
             <div className="container mx-auto items-center justify-between hidden lg:flex">
                 <a href="/" className="">
